1a629cd2e1d2a923bce70c30bd8e22d1b123a175
romanusovich <romanusovich1@gmail.com>
2023-01-14 17:05:27 +0300

    feat: add reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function reverse (n) {
-  
+    if (n < 0)
+        n *= -1;
+    
+    if (n >= 0 && n <= 9)
+        return n;
+
+    var tens = tenCountInNumber(n);
+
+    var result = 0;
+    for (var i = tens; i > 0; i--) {
+        var digit = Math.floor(n % 10);
+        n /= 10;
+        result += digit * 10 ** i;
+    }
+
+    return result + Math.floor(n);
+}
+
+function tenCountInNumber(n) {
+    var tens = 0;
+    while (n >= 10) {
+        n /= 10;
+        tens++;
+    }
+    return tens;
 }
