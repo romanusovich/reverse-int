725baf917e4ad6dca95039e5a6626528bb90f70b
roman <romanusovich1@gmail.com>
2026-04-06 20:07:49 +0200

    feat: complete reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
-module.exports = function reverse(/* n */) {
-  throw new Error('Not implemented');
+function tenCountInNumber(n) {
+  let tens = 0;
+  let temp = n;
+  while (temp >= 10) {
+    temp /= 10;
+    tens += 1;
+  }
+  return tens;
+}
+
+module.exports = function reverse(n) {
+  let temp = n;
+  if (temp < 0) temp *= -1;
+
+  if (temp >= 0 && temp <= 9) return temp;
+
+  const tens = tenCountInNumber(temp);
+
+  let result = 0;
+  for (let i = tens; i > 0; i -= 1) {
+    const digit = Math.floor(temp % 10);
+    temp /= 10;
+    result += digit * 10 ** i;
+  }
+
+  return result + Math.floor(temp);
 };
